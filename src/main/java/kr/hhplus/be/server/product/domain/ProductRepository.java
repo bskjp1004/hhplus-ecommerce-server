6540e75d507e3a0ecc8c33e28b56b7ff26addc89
KimJeongPil <bskjp1004@gmail.com>
2025-07-23 01:32:47 +0900

    feat: ProductRepository 인터페이스 및 임시 구현체(인메모리) 작성

---
+++ src/main/java/kr/hhplus/be/server/product/domain/ProductRepository.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.product.domain;
+
+import java.util.Optional;
+
+public interface ProductRepository {
+    Optional<Product> findById(long productId);
+}
---
+++ src/main/java/kr/hhplus/be/server/product/infra/ProductRepositoryImpl.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.product.infra;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import kr.hhplus.be.server.product.domain.Product;
+import kr.hhplus.be.server.product.domain.ProductRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+
+    // region
+    /**
+     * TODO - 실제 DB 연결 후 삭제하기
+     * */
+    private final Map<Long, Product> storage = new HashMap<>();
+    public ProductRepositoryImpl() {
+        storage.put(1L, Product.builder().id(1L).price(new BigDecimal("1000")).stock(10L).build());
+        storage.put(2L, Product.builder().id(2L).price(new BigDecimal("1500")).stock(5L).build());
+    }
+    // endregion
+
+    @Override
+    public Optional<Product> findById(long productId) {
+        return Optional.of(storage.get(productId));
+    }
+}
