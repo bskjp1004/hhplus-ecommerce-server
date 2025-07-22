aa75986c780828c79ffbbbb2946f0fbf3953ade3
KimJeongPil <bskjp1004@gmail.com>
2025-07-23 01:29:30 +0900

    feat: 비즈니스 에러 정의 및 `ErrorCode` 작성

---
+++ src/main/java/kr/hhplus/be/server/config/error/ErrorCode.java
@@ -0,0 +1,14 @@
+package kr.hhplus.be.server.config.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus status;
+}
