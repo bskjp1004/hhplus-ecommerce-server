0ab885d6b1ea1d098cc04789a972be8a6dd568da
jenny <bskjp1004@gmail.com>
2025-07-23 23:46:39 +0900

    refactor: Repository 구현체에서 `Product` → `ProductEntity` 교체

--- src/main/java/kr/hhplus/be/server/product/infra/ProductInMemoryRepository.java
+++ src/main/java/kr/hhplus/be/server/product/infra/ProductInMemoryRepository.java
@@ -1,26 +1,16 @@
 package kr.hhplus.be.server.product.infra;
 
-import java.math.BigDecimal;
+import kr.hhplus.be.server.product.domain.Product;
+import kr.hhplus.be.server.product.domain.ProductRepository;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import kr.hhplus.be.server.product.domain.Product;
-import kr.hhplus.be.server.product.domain.ProductRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductInMemoryRepository implements ProductRepository {
 
-    // region
-    /**
-     * TODO - 실제 DB 연결 후 삭제하기
-     * */
-    private final Map<Long, Product> storage = new HashMap<>();
-    public ProductInMemoryRepository() {
-        storage.put(1L, Product.builder().id(1L).price(new BigDecimal("1000")).stock(10L).build());
-        storage.put(2L, Product.builder().id(2L).price(new BigDecimal("1500")).stock(5L).build());
-    }
-    // endregion
+    private final Map<Long, ProductEntity> storage = new HashMap<>();
 
     @Override
     public Optional<Product> findById(long productId) {
