803032520164430eaf139acd517bf4467507a109
KimJeongPil <bskjp1004@gmail.com>
2025-07-24 19:00:47 +0900

    feat: `ErrorCode`에 존재하지 않는 유저 에러 코드 추가

--- src/main/java/kr/hhplus/be/server/config/error/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/config/error/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
