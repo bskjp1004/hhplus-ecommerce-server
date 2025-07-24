4d9aea6e39d1e0c6306aaf8d7587a5cfd89bba8e
KimJeongPil <bskjp1004@gmail.com>
2025-07-24 18:59:52 +0900

    feat: <잔액충전 기능> UserService 인터페이스 및 구현체 작성

---
+++ src/main/java/kr/hhplus/be/server/user/application/UserServiceAdapter.java
@@ -0,0 +1,30 @@
+package kr.hhplus.be.server.user.application;
+
+import java.util.Optional;
+import kr.hhplus.be.server.config.error.BusinessException;
+import kr.hhplus.be.server.config.error.ErrorCode;
+import kr.hhplus.be.server.user.application.dto.UserResponseDto;
+import kr.hhplus.be.server.user.domain.User;
+import kr.hhplus.be.server.user.domain.UserRepository;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceAdapter implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponseDto chargeBalance(long userId, BigDecimal amount) {
+        User originalUser = userRepository.findById(userId)
+            .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        User updatedUser = originalUser.chargeBalance(amount);
+
+        userRepository.insertOrUpdate(updatedUser);
+
+        return UserResponseDto.from(updatedUser);
+    }
+}
