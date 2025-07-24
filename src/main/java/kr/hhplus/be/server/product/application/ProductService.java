4e55d3eb0a3148e455b618f91867975507c1469a
KimJeongPil <bskjp1004@gmail.com>
2025-07-25 05:13:43 +0900

    feat: (상품) 다수 상품(도메인) 조회 기능 추가

--- src/main/java/kr/hhplus/be/server/product/application/ProductService.java
+++ src/main/java/kr/hhplus/be/server/product/application/ProductService.java
@@ -1,7 +1,14 @@
 package kr.hhplus.be.server.product.application;
 
+import java.util.List;
+import kr.hhplus.be.server.order.application.dto.OrderItemRequestDto;
 import kr.hhplus.be.server.product.application.dto.ProductResponseDto;
+import kr.hhplus.be.server.product.domain.Product;
 
 public interface ProductService {
+    Product getProductDomain(long productId);
+    List<Product> getProductDomains(List<Long> productIds);
     ProductResponseDto getProduct(long productId);
+    Product decreaseProductStock(OrderItemRequestDto request);
+    List<Product> decreaseProductStocks(List<OrderItemRequestDto> requests);
 }
