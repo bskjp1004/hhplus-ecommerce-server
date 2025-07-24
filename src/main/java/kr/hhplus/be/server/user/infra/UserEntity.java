5c1d14c74c0cd411bd367e2f86551dfd42ebece3
KimJeongPil <bskjp1004@gmail.com>
2025-07-24 18:49:00 +0900

    feat: `UserEntity` 작성

---
+++ src/main/java/kr/hhplus/be/server/user/infra/UserEntity.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.user.infra;
+
+import kr.hhplus.be.server.user.domain.User;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+public class UserEntity {
+    private long id;
+    private BigDecimal balance;
+
+    public User toDomain(){
+        return User.builder()
+            .id(this.id)
+            .balance(this.balance)
+            .build();
+    }
+
+    public static UserEntity fromDomain(User user){
+        return new UserEntity(
+            user.getId(),
+            user.getBalance()
+        );
+    }
+}
