f30a0056e435d891260aa7a9d819ea6dd743a1d0
KimJeongPil <bskjp1004@gmail.com>
2025-07-25 05:07:53 +0900

    feat: (쿠폰) 에러 코드 및 예외 처리 추가

--- src/main/java/kr/hhplus/be/server/config/error/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/config/error/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    COUPON_POLICY_NOT_FOUND("쿠폰정책을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    COUPON_NOT_FOUND("쿠폰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
